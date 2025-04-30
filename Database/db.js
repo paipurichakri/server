9e37db9105aa71d4cd7b3087726f404919bb7fc0
paipurichakri <chakrip0606@gmail.com>
2025-04-30 18:21:21 +0530

    first commit

--- Database/db.js
+++ Database/db.js
@@ -1,3 +1,2 @@
 let mongoose = require("mongoose");
-const DB_PATH ="mongodb+srv://chakrip0606:JbhKWeJx502NuNc3@cluster0.mcb4oxj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-mongoose.connect(DB_PATH);
+mongoose.connect("mongodb://localhost:27017/interview")
